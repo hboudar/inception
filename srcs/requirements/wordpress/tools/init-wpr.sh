a9b8a313098b263937597b1ca52abc2b391a3667
hboudar <boudarhamza12@gmail.com>
2025-05-25 16:59:00 +0100

    all seems to be right but nothing appear in browser

--- srcs/requirements/wordpress/tools/init-wpr.sh
+++ srcs/requirements/wordpress/tools/init-wpr.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-
-cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
-wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
-wp core install --url=$DOMAIN --title=$TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
-wp user create $USER_NAME $USER_EMAIL --role=author --user_pass=$USER_PASS --allow-root
-
-chmod -R 777 /var/www/html/ 
-php-fpm7.4 -F
-
-
-
-
-------last one
-#!/bin/bash
 set -e
 
 # Wait for MariaDB to be ready
